065419a4f748643e69b964276e7cbd4bc7ffc407
Nickleus Jimenez <nickleus.jimenez@gmail.com>
2015-04-08 09:00:30 +0800

    Initial Project Creation for word play

---
+++ MaterialAnagram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaterialAnagram.Startup))]
+namespace MaterialAnagram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
